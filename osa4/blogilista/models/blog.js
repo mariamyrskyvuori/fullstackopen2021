95a1b6fad62d7c508dc728abe5603851b3da0c01
Maria Myrskyvuori <mariamyrskyvuori@gmail.com>
2021-04-15 12:39:20 +0300

    4.8, supertesti ei toimi

--- osa4/blogilista/models/blog.js
+++ osa4/blogilista/models/blog.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 const uniqueValidator = require('mongoose-unique-validator')
 
-const url = process.env.MONGODB_URI
+/*const url = process.env.MONGODB_URI
 
 console.log('connecting to', url)
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
@@ -12,7 +12,7 @@ mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFind
   .catch((error) => {
     console.log('error connecting to MongoDB:', error.message)
   })
-
+*/
 
 const blogSchema = new mongoose.Schema({
   title: { type: String, minlength: 3, unique: true },
@@ -32,3 +32,6 @@ blogSchema.set('toJSON', {
 })
 
 module.exports = mongoose.model('Blog', blogSchema)
+
+
+
---
+++ osa4/blogilista/utils/list_helper.js
@@ -0,0 +1,54 @@
+const _ = require('lodash')
+const array = require('lodash/array')
+const object = require('lodash/fp/object')
+
+const dummy = (blogs) => {
+  const reducer = (sum, item) => {
+    return sum + item
+  }
+  //return array.reduce(reducer, 0) / array.length
+  return 1
+}
+
+
+const totalLikes = (blogs) => {
+  const allLikes = (sum, blog) => {
+    return sum + blog.likes
+  }
+  return blogs.reduce(allLikes, 0)
+
+}
+
+const favoriteBlog = (blogs) => {
+  const mostLikes = blogs.reduce(function (prev, current) {
+    return (prev.likes > current.likes) ? prev : current
+  })
+  return {
+    title: mostLikes.title,
+    author: mostLikes.author,
+    likes: mostLikes.likes
+  }
+}
+
+const mostBlogs = (blogs) => {
+  const newArray = _.map(blogs, 'author')
+  const hasMostBlogs = _.chain(newArray).countBy().toPairs().max(_.last).head().value()
+  const howManyBlogs = blogs.filter(function(blog) {
+    return blog.author === hasMostBlogs.toString()
+  })
+  console.log(hasMostBlogs)
+
+  return {
+    author: hasMostBlogs,
+    blogs: howManyBlogs.length
+  }
+}
+
+
+module.exports = {
+  dummy,
+  totalLikes,
+  favoriteBlog,
+  mostBlogs,
+}
+
